8c918db9372782ab363446c397fe377ed3ffb97d
robertoamaralprojetos <rcalvanodoamaral@gmail.com>
2026-09-04 19:29:19 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // sem internet). Estratégia: cache-first para assets estáticos, com atualização em
 // segundo plano; index.html sempre disponível offline como fallback de navegação.
 
-const CACHE_VERSION = 'metodo-pleno-v12';
+const CACHE_VERSION = 'metodo-pleno-v13';
 const PRECACHE_URLS = [
   './',
   './index.html',
